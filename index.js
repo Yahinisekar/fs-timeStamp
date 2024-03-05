6e63e08a0390c1947386d981abe0dc1754834dff
Yahini <136244447+Yahinisekar@users.noreply.github.com>
2024-03-05 08:53:04 +0530

    new

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ let date = format(new Date(), " dd-MM-yyyy HH-mm-ss");
 const filePath = `timeStamp/${date}.txt`;
   
 app.get('/', (req, res) => {
-  res.status(200).json({ "message": 'Welcome to the Nodejs file system' },
-  {endPoint:'/write'},{endPoint:"/read"});
+  res.status(200).json({ "message": 'Welcome to the Nodejs file system' });
 })
 app.get("/write", (req, res) => {
     try {
